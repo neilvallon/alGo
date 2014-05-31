669e6cf65dc7cc2dcf8b5d21204906f89b898bdf
Neil Vallon <neilvallon@gmail.com>
2014-05-31 12:32:36 -0500

    Add QuickSelect algorithm (Non-randomized)

---
+++ sort/quickselect.go
@@ -0,0 +1,27 @@
+package sort
+
+func QuickSelect(lst []int, ith int) int {
+	if ith <= 0 || len(lst) < ith {
+		panic("Selection element out of range.")
+	}
+
+	p := lst[0]
+	j := 1
+	for i := 1; i < len(lst); i++ {
+		if lst[i] < p {
+			lst[j], lst[i] = lst[i], lst[j]
+			j++
+		}
+	}
+
+	switch {
+	case ith < j:
+		return QuickSelect(lst[1:j], ith)
+	case ith > j:
+		return QuickSelect(lst[j:], ith-j)
+	case ith == j:
+		return p
+	}
+
+	panic("Something went horribly wrong")
+}
